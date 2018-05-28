cc915f443cbaed63fafe2fa183fd696a48db828d
DESKTOP-MR9Q9JP\melvin <guntanis666@hotmail.com>
2018-05-27 22:59:40 -0600

    initial commit

---
+++ src/main/java/tests.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class tests
+{
+    @Test
+    public static void  tests() {
+
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("range")
+    void testWithRangeMethodSource(int argument) {
+        assertNotEquals(9, argument);
+    }
+
+    static IntStream range() {
+        return IntStream.range(0, 20).skip(10);
+    }
+}
